60477453b6629ee3e60a0d04634feaf5837756c0
ryouta25 <avp22.07@mail.ru>
2026-06-27 09:33:44 +0300

    add main.cpp

--- pozdeev.andrey/F0/main.cpp
+++ pozdeev.andrey/F0/main.cpp
@@ -1,2 +1,23 @@
+#include "application.hpp"
+
+#include <iostream>
+#include <exception>
+
 int main()
-{}
+{
+  try {
+    pozdeev::Application app;
+    app.run();
+  } catch (const std::invalid_argument& ex) {
+    std::cerr << "Input Error: " << ex.what() << "\n";
+    return 1;
+  } catch (const std::exception& ex) {
+    std::cerr << "Fatal Error: " << ex.what() << "\n";
+    return 2;
+  } catch (...) {
+    std::cerr << "Unknown Fatal Error.\n";
+    return 2;
+  }
+
+  return 0;
+}
