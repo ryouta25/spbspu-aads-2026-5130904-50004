61727fb16a2999df89ebed5f6edd7bf143dc5d49
ryouta25 <avp22.07@mail.ru>
2026-06-27 09:07:56 +0300

    add Avl rotation mechanics and height calculation utilities

--- pozdeev.andrey/F0/avl_tree.hpp
+++ pozdeev.andrey/F0/avl_tree.hpp
@@ -51,6 +51,43 @@ namespace pozdeev {
 
     std::unique_ptr<Node> root_;
     size_t size_ = 0;
+
+    int height(Node* node) const
+    {
+      return node ? node->height_ : 0;
+    }
+
+    int getBalance(Node* node) const
+    {
+      return node ? height(node->left_.get()) - height(node->right_.get()) : 0;
+    }
+
+    void updateHeight(Node* node)
+    {
+      if (node) {
+        node->height_ = 1 + std::max(height(node->left_.get()), height(node->right_.get()));
+      }
+    }
+
+    std::unique_ptr<Node> rotateRight(std::unique_ptr<Node> y)
+    {
+      std::unique_ptr<Node> x = std::move(y->left_);
+      y->left_ = std::move(x->right_);
+      updateHeight(y.get());
+      x->right_ = std::move(y);
+      updateHeight(x.get());
+      return x;
+    }
+
+    std::unique_ptr<Node> rotateLeft(std::unique_ptr<Node> x)
+    {
+      std::unique_ptr<Node> y = std::move(x->right_);
+      x->right_ = std::move(y->left_);
+      updateHeight(x.get());
+      y->left_ = std::move(x);
+      updateHeight(y.get());
+      return y;
+    }
   };
 
 }
