4c42b768aea7c95028760df289e7ef65175ea8e3
ryouta25 <avp22.07@mail.ru>
2026-06-30 05:51:37 +0300

    переделал тесты

--- pozdeev.andrey/F0/test-avl_tree.cpp
+++ pozdeev.andrey/F0/test-avl_tree.cpp
@@ -1,54 +1,52 @@
 #include <boost/test/unit_test.hpp>
+#include <string>
 
 #include "avl_tree.hpp"
 
-#include <string>
+BOOST_AUTO_TEST_SUITE(AvlTree_tests)
 
-BOOST_AUTO_TEST_SUITE(AvlTreeTests)
+using Tree = pozdeev::AvlTree<std::string, int>;
 
-BOOST_AUTO_TEST_CASE(testInsertAndFind)
+BOOST_AUTO_TEST_CASE(test_insert_and_find)
 {
-  pozdeev::AvlTree<std::string, int> tree;
+  Tree tree;
 
-  BOOST_CHECK(tree.insert("scenario_a", 100));
-  BOOST_CHECK(tree.insert("scenario_b", 200));
+  BOOST_CHECK(tree.getSize() == 0);
 
-  BOOST_CHECK(!tree.insert("scenario_a", 300));
-
-  int* valA = tree.find("scenario_a");
-  BOOST_REQUIRE(valA != nullptr);
-  BOOST_CHECK_EQUAL(*valA, 100);
-
-  int* valC = tree.find("scenario_c");
-  BOOST_CHECK(valC == nullptr);
-}
-
-BOOST_AUTO_TEST_CASE(testClearAndSize)
-{
-  pozdeev::AvlTree<int, std::string> tree;
-
-  BOOST_CHECK_EQUAL(tree.getSize(), 0);
-
-  tree.insert(1, "one");
-  tree.insert(2, "two");
-  tree.insert(3, "three");
+  BOOST_CHECK(tree.insert("scenario_a", 10) == true);
+  BOOST_CHECK(tree.insert("scenario_b", 20) == true);
+  BOOST_CHECK(tree.insert("scenario_c", 30) == true);
 
   BOOST_CHECK_EQUAL(tree.getSize(), 3);
 
-  tree.clear();
-  BOOST_CHECK_EQUAL(tree.getSize(), 0);
-  BOOST_CHECK(tree.find(1) == nullptr);
+  auto itA = tree.find("scenario_a");
+  BOOST_CHECK(itA != tree.end());
+  BOOST_CHECK_EQUAL(*itA, 10);
+
+  auto itC = tree.find("scenario_c");
+  BOOST_CHECK(itC != tree.end());
+  BOOST_CHECK_EQUAL(*itC, 30);
+
+  auto itNotFound = tree.find("unknown");
+  BOOST_CHECK(itNotFound == tree.end());
+
+  BOOST_CHECK(tree.insert("scenario_a", 99) == false);
+  BOOST_CHECK_EQUAL(tree.getSize(), 3);
 }
 
-BOOST_AUTO_TEST_CASE(testBalancingAndHeight)
+BOOST_AUTO_TEST_CASE(test_clear)
 {
-  pozdeev::AvlTree<int, int> tree;
+  Tree tree;
 
-  tree.insert(1, 10);
-  tree.insert(2, 20);
-  tree.insert(3, 30);
+  tree.insert("a", 1);
+  tree.insert("b", 2);
 
-  BOOST_CHECK_EQUAL(tree.getHeight(), 2);
+  BOOST_CHECK_EQUAL(tree.getSize(), 2);
+
+  tree.clear();
+
+  BOOST_CHECK_EQUAL(tree.getSize(), 0);
+  BOOST_CHECK(tree.find("a") == tree.end());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- pozdeev.andrey/F0/test-scenario.cpp
+++ pozdeev.andrey/F0/test-scenario.cpp
@@ -1,51 +1,66 @@
 #include <boost/test/unit_test.hpp>
+#include <string>
+#include <cmath>
 
 #include "scenario.hpp"
 
-BOOST_AUTO_TEST_SUITE(ScenarioTests)
+BOOST_AUTO_TEST_SUITE(Scenario_tests)
 
-BOOST_AUTO_TEST_CASE(testScenarioCreation)
+using Scenario = pozdeev::Scenario;
+using action_t = pozdeev::action_t;
+
+BOOST_AUTO_TEST_CASE(test_add_action_sorting)
 {
-  pozdeev::Scenario sc("navi_dust2", "grand_final", "NaVi", "Dust2", "Execute");
+  Scenario sc("navi_dust2", "grand_final", "NaVi", "Dust2", "Execute");
 
-  BOOST_CHECK_EQUAL(sc.getId(), "navi_dust2");
-  BOOST_CHECK_EQUAL(sc.getMatch(), "grand_final");
-  BOOST_CHECK_EQUAL(sc.getTeam(), "NaVi");
-  BOOST_CHECK_EQUAL(sc.getMap(), "Dust2");
-  BOOST_CHECK_EQUAL(sc.getType(), "Execute");
-}
-
-BOOST_AUTO_TEST_CASE(testAddActionSorting)
-{
-  pozdeev::Scenario sc("id1", "m1", "t1", "map", "type");
-
-  pozdeev::action_t actionLate{2.5, "s1mple", "MOVE", "long", 1.0};
-  pozdeev::action_t actionEarly{1.0, "b1t", "THROW_SMOKE", "doors", 0.9};
-
-  sc.addAction(actionLate);
-  sc.addAction(actionEarly);
-
-  const auto& actions = sc.getActions();
-  BOOST_REQUIRE_EQUAL(actions.getSize(), 2);
-
-  BOOST_CHECK_EQUAL(actions[0].time_, 1.0);
-  BOOST_CHECK_EQUAL(actions[1].time_, 2.5);
-  BOOST_CHECK_EQUAL(actions[0].player_, "b1t");
-}
-
-BOOST_AUTO_TEST_CASE(testWinProbabilityCalculation)
-{
-  pozdeev::Scenario sc("id1", "m1", "t1", "map", "type");
-
-  BOOST_CHECK_CLOSE(sc.calculateWinProbability(), 0.0, 1e-5);
-
-  pozdeev::action_t a1{1.0, "p1", "MOVE", "a", 0.90};
-  pozdeev::action_t a2{2.0, "p2", "SHOOT", "b", 0.80};
+  action_t a1{2.5, "s1mple", "MOVE", "long", 1.0};
+  action_t a2{1.0, "b1t", "THROW_SMOKE", "doors", 0.9};
 
   sc.addAction(a1);
   sc.addAction(a2);
 
-  BOOST_CHECK_CLOSE(sc.calculateWinProbability(), 0.72, 1e-5);
+  const auto& actions = sc.getActions();
+  BOOST_CHECK_EQUAL(actions.getSize(), 2);
+
+  BOOST_CHECK_EQUAL(actions[0].time, 1.0);
+  BOOST_CHECK_EQUAL(actions[1].time, 2.5);
+  BOOST_CHECK_EQUAL(actions[0].player, "b1t");
+  BOOST_CHECK_EQUAL(actions[1].player, "s1mple");
+}
+
+BOOST_AUTO_TEST_CASE(test_calculate_win_probability)
+{
+  Scenario sc("test", "test", "test", "test", "test");
+
+  BOOST_CHECK(std::abs(sc.calculateWinProbability() - 1.0) < 0.0001);
+
+  action_t a1{1.0, "p1", "type", "target", 0.9};
+  action_t a2{2.0, "p2", "type", "target", 0.8};
+
+  sc.addAction(a1);
+  sc.addAction(a2);
+
+  BOOST_CHECK(std::abs(sc.calculateWinProbability() - 0.72) < 0.0001);
+}
+
+BOOST_AUTO_TEST_CASE(test_analyze_weakness)
+{
+  Scenario sc("test", "test", "test", "test", "test");
+
+  action_t a1{1.0, "p1", "type", "target", 0.9};
+  action_t a2{15.0, "weak_player", "type", "target", 0.1};
+  action_t a3{30.0, "p3", "type", "target", 0.8};
+
+  sc.addAction(a1);
+  sc.addAction(a2);
+  sc.addAction(a3);
+
+  pozdeev::AnalysisResult result = sc.analyzeWeakness();
+
+  BOOST_CHECK(result.hasActions == true);
+  BOOST_CHECK(std::abs(result.minProb - 0.1) < 0.0001);
+  BOOST_CHECK_EQUAL(result.weakPlayer, "weak_player");
+  BOOST_CHECK_EQUAL(result.weakTime, 15.0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
