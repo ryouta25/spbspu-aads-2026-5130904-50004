61280beb2f5d5d841e0ea973027a60463057d252
ryouta25 <avp22.07@mail.ru>
2026-06-30 05:11:35 +0300

    refactor(scenario): отделение логики от ввода-вывода и исправление CG. - Из структуры action_t убраны суффиксы '_' у полей (согласно CG для открытых структур) - Добавлена структура AnalysisResult для возврата результатов анализа без прямого вывода в консоль - Добавлена перегрузка operator<< для чистого форматированного вывода метаданных сценария

--- pozdeev.andrey/F0/scenario.hpp
+++ pozdeev.andrey/F0/scenario.hpp
@@ -2,24 +2,35 @@
 #define SCENARIO_HPP
 
 #include "vector.hpp"
-
 #include <string>
+#include <iostream>
 
 namespace pozdeev {
 
   struct action_t {
-    double time_;
-    std::string player_;
-    std::string type_;
-    std::string target_;
-    double prob_;
+    double time;
+    std::string player;
+    std::string type;
+    std::string target;
+    double prob;
+  };
+
+  struct AnalysisResult {
+    bool hasActions;
+    double totalProb;
+    double minProb;
+    double weakTime;
+    std::string weakPlayer;
+    std::string weakType;
+    double impactProb;
   };
 
   class Scenario {
   public:
     Scenario() = default;
-    Scenario(const std::string& id, const std::string& match, const std::string& team,
-             const std::string& map, const std::string& type);
+    Scenario(const std::string& id, const std::string& match,
+             const std::string& team, const std::string& map,
+             const std::string& type);
 
     const std::string& getId() const;
     const std::string& getMatch() const;
@@ -29,9 +40,10 @@ namespace pozdeev {
     const Vector<action_t>& getActions() const;
 
     void addAction(const action_t& action);
-    void printSimulation() const;
     double calculateWinProbability() const;
-    void analyzeWeakness() const;
+    AnalysisResult analyzeWeakness() const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Scenario& sc);
 
   private:
     std::string id_;
